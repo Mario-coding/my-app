c97e991a62a921ca94012f8010e46dbf5f72eba7
Mario-coding <laurenmario19@gmail.com>
2021-03-20 10:15:20 +0700

    message

--- src/index.tsx
+++ src/index.tsx
@@ -4,13 +4,25 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const dataDummy = [
+  {
+    id: "1",
+    name: "John",
+    email: "john@gmail.com"
+  },
+  {
+    id: "1",
+    name: "frau",
+    email: "frau@gmail.com"
+  },
+]
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App data={dataDummy} />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
